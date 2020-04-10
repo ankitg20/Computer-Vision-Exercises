0af45a82e983528c5968fce2c1be381a4e75c688
Ankit Gupta <ankitg20@stanford.edu>
2020-04-10 14:43:01 -0400

    Add files via upload

---
+++ CannyEdgeDetection/cmake-build-debug/CMakeFiles/CannyEdgeDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CannyEdgeDetection.dir/l52.cpp.o"
+  "CannyEdgeDetection"
+  "CannyEdgeDetection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CannyEdgeDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
